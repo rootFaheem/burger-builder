0fb279527baa29426868ccea58ab623691849bea
faheem <faheem@faheems-MacBook-Air.local>
2019-01-06 14:49:32 +0530

    Dynamic addition of ingredients has been added

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,15 +3,16 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-    // const transformedIngredient = Object.keys(props.ingredients)
-    //     .map(igKey => {
-    //         return [...Array(props.ingredients[igKey])]
-    //     });
+    const transformedIngredient = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey} />; 
+            });
+        });
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {transformedIngredient}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
