b7bfae7415fbf0ccf2dc3701948a8292f1004ff5
faheem <faheem@faheems-MacBook-Air.local>
2019-01-06 18:02:37 +0530

    ORDER NOW button has been added and working for all test-cases

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -11,13 +11,19 @@ const controls = [
 
 const buildControls = (props) => (
     <div className={classes.BuildControls}>
+    <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
         {controls.map(ctrl => (
             <BuildControl 
                 key={ctrl.label} 
                 label={ctrl.label} 
                 added={() => props.ingredientAdded(ctrl.type)}
-                removed={() => props.ingredientRemoved(ctrl.type)}/>
+                removed={() => props.ingredientRemoved(ctrl.type)}
+                disabled={props.disabled[ctrl.type]}/>
         ))}
+        <button 
+            className={classes.OrderButton}
+            disabled={!props.purchasable}> ORDER NOW
+        </button>
     </div>
 );
 
