6dcd3ea1b5e23b51be7b0dfcf83659b98e9ba9ec
faheem <faheem@faheems-MacBook-Air.local>
2019-01-07 22:22:09 +0530

    Humburger has been added for SideDrawer, App has been finalized for Version 1.0

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import classes from './DrawerToggle.css';
 
 const drawerToggle = (props) => (
-    <div onClick={props.clicked}>MENU</div>
+    <div className={classes.DrawerToggle} onClick={props.clicked}>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
 );
 
 export default drawerToggle;
