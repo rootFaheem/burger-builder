119bd751f10159fa3aed1fa2f4fed30266a92880
Faheem <faheemtech1@gmail.com>
2019-01-25 22:06:27 +0530

    axios is working for post request

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from  '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -86,7 +87,24 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('You have purchased a Burger');
+        // alert('You have purchased a Burger');
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Faheem',
+                address: {
+                    street: 'Nafees Road',
+                    zipcode: '110025',
+                    country: 'INDIA'
+                },
+                email: 'faheem@gmail.com'
+            },
+            deliveryMethod: 'fastest'
+                }
+        axios.post('/orders.json', order)
+        .then(response => console.log(response))
+        .catch(error => console.log(error));
     }
 
     render () {
