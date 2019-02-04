6ad5120f1cfaee06e39bdf5d9941b6f8e09c8b1b
Faheem <faheemtech1@gmail.com>
2019-02-04 12:37:14 +0530

    ingredients passed by Query params

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -12,6 +12,15 @@ class Checkout extends Component {
     }
   };
 
+  componentDidMount() {
+    const query = new URLSearchParams(this.props.location.search);
+    const ingredients = {};
+    for (let param of query.entries()) {
+      ingredients[param[0]] = +param[1];
+    }
+    this.setState({ ingredients });
+  }
+
   checkoutCancelledHandler = () => {
     this.props.history.goBack();
   };
