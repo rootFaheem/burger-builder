1d1ce7b6c0e95071eccda3a0f373308d69c01bc4
Faheem <faheemtech1@gmail.com>
2019-02-07 11:41:20 +0530

    react-redux added to the project

---
+++ src/store/reducer.js
@@ -0,0 +1,12 @@
+import * as actionType from "./actions";
+
+const initialState = {
+  ingredients: null,
+  totalPrice: 4
+};
+
+const reducer = ((state = initialState), action) => {
+
+};
+
+export default reducer;
