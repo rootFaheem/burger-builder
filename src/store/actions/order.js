a5496a6f6f7d203303aab1eca7db63de8ec70553
Faheem <faheemtech1@gmail.com>
2019-02-08 20:41:07 +0530

    adding Order Actions

---
+++ src/store/actions/order.js
@@ -0,0 +1,31 @@
+import * as actionTypes from "./actionTypes";
+import axios from "../../axios-orders";
+
+export const purchaseBurgerSuccess = (id, orderData) => {
+  return {
+    type: actionTypes.PPURCHASE_BURGER_SUCCESS,
+    orderId: id,
+    orderData: orderData
+  };
+};
+
+export const purchaseBurgerFail = error => {
+  return {
+    type: actionTypes.PURCHASE_BURGER_FAIL,
+    error: error
+  };
+};
+
+export const puchaseBurgerStart = orderData => {
+  return dispatch => {
+    axios
+      .post("/orders.json", orderData)
+      .then(response => {
+        console.log(response.data);
+        dispatch(purchaseBurgerSuccess(response.data, orderData));
+      })
+      .catch(error => {
+        dispatch(purchaseBurgerFail(error));
+      });
+  };
+};
