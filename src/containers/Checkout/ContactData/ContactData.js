be1db7cff26a109082af07ab6a0f3385034c303b
Faheem <faheemtech1@gmail.com>
2019-02-08 21:01:20 +0530

    connecting ContactData container actions

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -6,6 +6,8 @@ import Spinner from "../../../components/UI/Spinner/Spinner";
 import classes from "./ContactData.css";
 import axios from "../../../axios-orders";
 import Input from "../../../components/UI/Input/Input";
+import withErrorHandler from "../../../hoc/withErrorHandler/withErrorHandler";
+import * as actions from "../../../store/actions/index";
 
 class ContactData extends Component {
   state = {
@@ -110,6 +112,8 @@ class ContactData extends Component {
       price: this.props.price,
       orderData: formData
     };
+
+    this.props.onOrderBurger(order);
   };
 
   checkValidity(value, rules) {
@@ -211,4 +215,8 @@ const mapStateToProps = state => {
   };
 };
 
-export default connect(mapStateToProps)(ContactData);
+const mapDispatchToProps = dispatch => {
+  onOrderBurger: orderData => dispatch(actions.purchaseBurgerStart(orderData));
+};
+
+export default connect(mapStateToProps)(withErrorHandler(ContactData, axios));
--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -4,4 +4,4 @@ export {
   initIngredients
 } from "./burgerBuilder";
 
-export {} from "./order";
+export { purchaseBurgerStart } from "./order";
