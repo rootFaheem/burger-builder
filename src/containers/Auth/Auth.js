32106ec705e66f105397b7ae02a52a197993f057
Faheem <faheemtech1@gmail.com>
2019-02-15 21:39:18 +0530

    form Auth Start is working in Redux

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 import Input from "../../components/UI/Input/Input";
 import Button from "../../components/UI/Button/Button";
 import classes from "./Auth.css";
+import * as actions from "../../store/actions/index";
 
 class Auth extends Component {
   state = {
@@ -70,10 +72,10 @@ class Auth extends Component {
   }
 
   inputChangedHandler = (event, controlName) => {
-    const updateControls = {
+    const updatedControls = {
       ...this.state.controls,
       [controlName]: {
-        ...this.state.controlName[controlName],
+        ...this.state.controls[controlName],
         value: event.target.value,
         valid: this.checkValidity(
           event.target.value,
@@ -82,8 +84,17 @@ class Auth extends Component {
         touched: true
       }
     };
-    this.setState({ controls: updateControls });
+    this.setState({ controls: updatedControls });
   };
+
+  submitHandler = event => {
+    event.preventDefault();
+    this.props.onAuth(
+      this.state.controls.email.value,
+      this.state.controls.password.value
+    );
+  };
+
   render() {
     const formElementsArray = [];
     for (let key in this.state.controls) {
@@ -92,6 +103,7 @@ class Auth extends Component {
         config: this.state.controls[key]
       });
     }
+
     const form = formElementsArray.map(formElement => (
       <Input
         key={formElement.id}
@@ -104,9 +116,10 @@ class Auth extends Component {
         changed={event => this.inputChangedHandler(event, formElement.id)}
       />
     ));
+
     return (
       <div className={classes.Auth}>
-        <form>
+        <form onSubmit={this.submitHandler}>
           {form}
           <Button btnType="Success">SUBMIT</Button>
         </form>
@@ -115,4 +128,13 @@ class Auth extends Component {
   }
 }
 
-export default Auth;
+const mapDispatchToProps = dispatch => {
+  return {
+    onAuth: (email, password) => dispatch(actions.auth(email, password))
+  };
+};
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(Auth);
--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,10 +1,8 @@
 export {
-    addIngredient,
-    removeIngredient,
-    initIngredients
-} from './burgerBuilder';
-export {
-    purchaseBurger,
-    purchaseInit,
-    fetchOrders
-} from './order';
+  addIngredient,
+  removeIngredient,
+  initIngredients
+} from "./burgerBuilder";
+export { purchaseBurger, purchaseInit, fetchOrders } from "./order";
+
+export { auth } from "./auth";
