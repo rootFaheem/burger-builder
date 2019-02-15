19e38031593b0eb46e559f4a3fd827c409257622
Faheem <faheemtech1@gmail.com>
2019-02-15 12:31:40 +0530

    Auth Form added

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 
 import Input from "../../components/UI/Input/Input";
 import Button from "../../components/UI/Button/Button";
+import classes from "./Auth.css";
 
 class Auth extends Component {
   state = {
@@ -36,12 +37,59 @@ class Auth extends Component {
       }
     }
   };
+
+  checkValidity(value, rules) {
+    let isValid = true;
+    if (!rules) {
+      return true;
+    }
+
+    if (rules.required) {
+      isValid = value.trim() !== "" && isValid;
+    }
+
+    if (rules.minLength) {
+      isValid = value.length >= rules.minLength && isValid;
+    }
+
+    if (rules.maxLength) {
+      isValid = value.length <= rules.maxLength && isValid;
+    }
+
+    if (rules.isEmail) {
+      const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+      isValid = pattern.test(value) && isValid;
+    }
+
+    if (rules.isNumeric) {
+      const pattern = /^\d+$/;
+      isValid = pattern.test(value) && isValid;
+    }
+
+    return isValid;
+  }
+
+  inputChangedHandler = (event, controlName) => {
+    const updateControls = {
+      ...this.state.controls,
+      [controlName]: {
+        ...this.state.controlName[controlName],
+        value: event.target.value,
+        valid: this.checkValidity(
+          event.target.value,
+          this.state.controls[controlName].validation
+        ),
+        touched: true
+      }
+    };
+    this.setState({ controls: updateControls });
+  };
   render() {
     const formElementsArray = [];
     for (let key in this.state.controls) {
       formElementsArray.push({
         id: key,
-        config: this.state.orderForm[key]
+        config: this.state.controls[key]
       });
     }
     const form = formElementsArray.map(formElement => (
@@ -57,7 +105,7 @@ class Auth extends Component {
       />
     ));
     return (
-      <div>
+      <div className={classes.Auth}>
         <form>
           {form}
           <Button btnType="Success">SUBMIT</Button>
