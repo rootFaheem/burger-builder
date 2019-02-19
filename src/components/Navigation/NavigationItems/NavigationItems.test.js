43a8678bd2f432b3b5c08b4c2579893e08b71470
Faheem <faheemtech1@gmail.com>
2019-02-19 16:21:57 +0530

    helper functions like beforeEach() and wrapper.setProps() used

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -9,13 +9,19 @@ import NavigationItem from "./NavigationItem/NavigationItem";
 configure({ adapter: new Adapter() });
 
 describe("<NavigationItems /> ", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<NavigationItems />);
+  });
+
   it("should render two <NavigationItem />  elements if not authenticated", () => {
-    const wrapper = shallow(<NavigationItems />);
     expect(wrapper.find(NavigationItem)).toHaveLength(2);
   });
 
   it("should render three <NavigationItem />  elements if authenticated", () => {
-    const wrapper = shallow(<NavigationItems isAuthenticated />);
+    // const wrapper = shallow(<NavigationItems isAuthenticated />);
+    wrapper.setProps({ isAuthenticated: true });
     expect(wrapper.find(NavigationItem)).toHaveLength(3);
   });
 });
