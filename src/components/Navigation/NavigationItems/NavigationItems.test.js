e5286b29f407e2d5ed0b229e6a59e3f9ce304e91
Faheem <faheemtech1@gmail.com>
2019-02-19 16:39:26 +0530

    adding test for /logout route

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -24,4 +24,11 @@ describe("<NavigationItems /> ", () => {
     wrapper.setProps({ isAuthenticated: true });
     expect(wrapper.find(NavigationItem)).toHaveLength(3);
   });
+
+  it("should render logout from <NavigationItem />  elements if authenticated", () => {
+    wrapper.setProps({ isAuthenticated: true });
+    expect(
+      wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)
+    ).toEqual(true);
+  });
 });
